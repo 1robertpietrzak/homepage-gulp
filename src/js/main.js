bc0c13407647766ca03d8740b3e60ddd518dd31d
Robert <robzii17@gmail.com>
2020-01-12 11:32:56 +0100

    DOrzucenie starego projektu

--- src/js/main.js
+++ src/js/main.js
@@ -14,7 +14,27 @@ if ('serviceWorker' in navigator) {
   });
 }
 
-// place your code below
+const thirdHeaderJs = document.querySelector('.home__pamietniczek--header-js');
+console.log(thirdHeaderJs);
+thirdHeaderJs.innerHTML = thirdHeaderJs.innerHTML + ' przez JS.'
+
+
+function promptt() {
+    let imie = prompt("Jak masz na imie?", "");
+    document.querySelector('.home__pamietniczek--header-js');
+    if (imie) {
+        console.log(imie);
+        thirdHeaderJs.innerHTML = thirdHeaderJs.innerHTML + ' Przegladasz super strone: ' + imie + ' .';
+    }
+}
+
+
+
+const homeNaviButton = document.querySelector('.home_miniNav--button--js');
+homeNaviButton.addEventListener('click', (e) => {
+    const homeNav = document.querySelector('.home__nav--js')
+    homeNav.classList.toggle('home__mininav');
+})
 
 
 console.log(`Hello world!`);
